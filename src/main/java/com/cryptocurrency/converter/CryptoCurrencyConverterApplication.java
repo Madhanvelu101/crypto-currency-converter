953d5accfe2a97acc036ef2e11a8c0b7dfffea33
cb-madhankumar <madhan.kumar@chargebee.com>
2023-05-19 12:31:55 +0530

    Initial Project setup

---
+++ src/main/java/com/cryptocurrency/converter/CryptoCurrencyConverterApplication.java
@@ -0,0 +1,13 @@
+package com.cryptocurrency.converter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoCurrencyConverterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptoCurrencyConverterApplication.class, args);
+	}
+
+}
