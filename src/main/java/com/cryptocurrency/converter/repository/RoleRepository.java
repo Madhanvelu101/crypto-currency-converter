ce0b0ccb1f431cb2aab5c0bcfe2f366afee5f0fa
cb-madhankumar <madhan.kumar@chargebee.com>
2023-05-27 12:05:27 +0530

    Repository class for User and Role

---
+++ src/main/java/com/cryptocurrency/converter/repository/RoleRepository.java
@@ -0,0 +1,7 @@
+package com.cryptocurrency.converter.repository;
+
+import com.cryptocurrency.converter.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Long>{
+}
---
+++ src/main/java/com/cryptocurrency/converter/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.cryptocurrency.converter.repository;
+
+import com.cryptocurrency.converter.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+}
